7e8f06cbc7eaa7924018d167e476ada902ac30b1
myfirstOne <meryem.elmchichi@gmail.com>
2020-04-14 00:19:00 +0200

    exe4.1

--- src/test/java/uvsq/m1/gl/AppTest.java
+++ src/test/java/uvsq/m1/gl/AppTest.java
@@ -1,5 +1,6 @@
 package uvsq.m1.gl;
 
+import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertTrue;
 
 import org.junit.Test;
@@ -17,4 +18,13 @@ public class AppTest
     {
         assertTrue( true );
     }
+    @Test
+    public void testPersonnel() {
+    	
+    	Personnel p1 = new Personnel.Builder("Marc", "Gomez", "Doctor").build();
+    	
+    	assertEquals("Marc", p1.getNom());
+    	
+    }
 }
+
